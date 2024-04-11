11ec5130ccb8a2c0e6757cc5f8df7af5673c0801
Pedro Alves Batista <pedro.pesquisapb@gmail.com>
2024-04-11 20:43:39 -0300

    Add folders structure to DataSink

---
+++ datasink/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "Welcome to DataSink";
+   cout << " ";
+   cout << "Would you like to search new devices ?";
+   
+   char op = ' ';
+   cin >> op;
+   switch (op)
+   {
+   case 'yes' || 'y' || 'Y':
+    /* code */
+    break;
+   
+   default:
+    break;
+   }
+
+
+}
